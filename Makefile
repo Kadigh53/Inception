02f8d0510b75921496d57e75ccf86d4c6bea1cc3
Abdelkader  Aoutem <abdlkadraoutem@gmail.com>
2024-04-27 12:00:47 +0100

    in progress

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: aaoutem- <aaoutem-@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/03/26 18:07:58 by aaoutem-          #+#    #+#              #
-#    Updated: 2024/04/27 02:43:52 by aaoutem-         ###   ########.fr        #
+#    Updated: 2024/04/27 11:11:00 by aaoutem-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,5 +15,5 @@ build:
 # docker image build -t nginx-img ./srcs/NGINX
 
 run:
-	docker container run --name mariadb mariadb-img
+	docker container run -it --name mariadb mariadb-img
 #docker container run -d -p 80:80 -p 443:443 --name nginx nginx-img
--- srcs/requirements/mariaDB/tools/createDB.sh
+++ srcs/requirements/mariaDB/tools/createDB.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-service mysql start
+service mariadb start;
 
 # Create a database
-mysql -u root -e "CREATE DATABASE IF NOT EXIST '$DB_NAME';"
-mysql -u root -e "CREATE USER IF NOT EXIST '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -u root -e "GRANT ALL PREVILIGES ON '$DB_NAME'.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+# mysql -uroot-e "GRANT ALL PRIVILEGES ON '$DB_NAME'.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'pass123';"
+mysql -u root -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$DB_PASSWORD shutdown
+mariadb-admin -u root -p$DB_PASSWORD shutdown
 
-exec mysqld_safe
+exec mariadbd-safe 
