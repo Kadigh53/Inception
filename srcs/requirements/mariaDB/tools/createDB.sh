8d9038d74ba8d873c22fcb43f5a1afd87b514384
Abdelkader  Aoutem <abdlkadraoutem@gmail.com>
2024-05-06 10:06:36 +0100

    wp in progress

--- srcs/requirements/mariaDB/tools/createDB.sh
+++ srcs/requirements/mariaDB/tools/createDB.sh
@@ -7,7 +7,7 @@ mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 # mysql -uroot-e "GRANT ALL PRIVILEGES ON '$DB_NAME'.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'pass123';"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
 mariadb-admin -u root -p$DB_PASSWORD shutdown
